124a887765ca5a6713db27ce0c2bc25576ca2b95
mrissa000 <mrissa000@gmail.com>
2026-09-10 16:01:44 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
+import { cloudfare } from '@cloudflare/vite-plugin'
 
 export default defineConfig({
   plugins: [react()],
